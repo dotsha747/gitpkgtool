9edcb78998230121c3df855d51a70fdfae9c6d9f
shahada abubakar (pc-shahada) <shahada@abubakar.net>
2017-12-31 01:02:56 +0800

    added publish script

---
+++ publish-deb.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# publish build packages to repository. These should be set in your
+# ~/.bashrc:
+# 	DEBREPOUSER
+#		DEBREPOHOST
+# 	DEBREPODIR
+
+scp $@ ${DEBREPOUSER}@${DEBREPOHOST}:${DEBREPODIR}/pool/main/
+ssh ${DEBREPOUSER}@${DEBREPOHOST} "cd ${DEBREPODIR}; ~/reindex_stable.sh"
